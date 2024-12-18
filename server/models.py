c3f49de7c618c62cb5f37c977f6766cd50a21a78
samuelgitau <amsamgitau@gmail.com>
2024-12-18 17:10:18 +0300

    added .env file and modified files

--- server/models.py
+++ server/models.py
@@ -13,11 +13,21 @@ class Employee(db.Model):
     __tablename__ = 'employees'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False) 
-    role = db.Column(db.String(120), nullable=False) 
+    role = db.Column(db.String(120), nullable=False, default="Attendant") 
     shift = db.Column(db.String(120), nullable=False)
     sales = db.Column(db.Float, nullable=False)
     salary = db.Column(db.Float, nullable=False)
     is_paid = db.Column(db.Boolean, nullable=False, default=False)
+    date_joined = db.Column(db.Date, nullable=False)
     
+
+class Sales(db.Mode):
+    __tablename__ = 'sales'
+    id = db.Column(db.Integer, primary_key=True)
+    fuel_type = db.Column(db.String(120), nullable=False)
+    pump_number = db.Column(db.Integer, nullable=False)
+    liters = db.Column(db.Float, nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'), nullable=False)
+        
     
     
